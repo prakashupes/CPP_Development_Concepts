aa996ebcb1eb0ecba4775fed9b535189ded06504
prakashupes <85prakash2017@gmail.com>
2020-02-06 23:15:15 +0530

    cpp

---
+++ CPP IN Action/003_accumulate().cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<numeric>
+using namespace std;
+//Backend of accumulate
+template <typename Iterator,typename INIT>
+INIT acc(Iterator first,Iterator sec,INIT sum)
+{
+    while(first!=sec)
+    {
+        sum+=*(first);
+        first++;
+    }
+    return sum;
+}
+
+
+int myFun(int a,int b)
+{
+    return a*b;
+}
+int main()
+{
+    int sum=1;
+    int arr[]={2,5,7};
+    cout<<accumulate(arr,arr+3,sum)<<endl; //First type
+
+    cout<<accumulate(arr,arr+3,sum,myFun)<<endl; //SeconType
+
+    cout<<acc(arr,arr+3,sum); //Using backend
+
+}
+
