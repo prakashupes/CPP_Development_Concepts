87e55682d808f3b8df9c99e80c625b68f11ba11a
prakashupes <85prakash2017@gmail.com>
2020-02-10 21:24:49 +0530

    cpp

---
+++ 1_OOPs concepts/007_InitializerList.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class Myclass
+{
+    int a,b;
+    Myclass(int x,int y) : a{x},b{y} {// Rest of the code,We can use ()in plce of{} but problem of narrowing
+
+    }
+
+};
+int main()
+{
+
+}
