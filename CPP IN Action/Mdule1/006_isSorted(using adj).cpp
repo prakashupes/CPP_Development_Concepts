0f405cc8022920529020ea6d04728559719472bd
prakashupes <85prakash2017@gmail.com>
2020-02-13 13:41:24 +0530

    cpp

--- CPP IN Action/Mdule1/006_isSorted(using adj).cpp
+++ CPP IN Action/Mdule1/006_isSorted(using adj).cpp
@@ -1,7 +1,13 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 int main()
 {
-
+    vector<int> v={1,7,2,3,4};
+    vector<int>::iterator it=adjacent_find(v.begin(),v.end(),greater<int>());
+   // cout<<*it;
+    if(it==v.end())
+        cout<<"Sorted";
 
 }
