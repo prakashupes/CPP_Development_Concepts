72c392eda80a17a9866c9fa76a26f8d7577578db
prakashupes <85prakash2017@gmail.com>
2020-02-13 13:36:35 +0530

    cpp

--- CPP IN Action/Mdule1/005_adjecentFind.cpp
+++ CPP IN Action/Mdule1/005_adjecentFind.cpp
@@ -3,13 +3,17 @@
 #include<algorithm>
 using namespace std;
 int main()
-
+//find all repeated elements in arr
 {
-    int arr[4]={1,2,3,3};
-    vector<int> v (arr,arr+8);
+    int arr[]={1,2,3,3,4,5,6,6,11,12,12,23,23};
+    vector<int> v (arr,arr+14);
 
     vector<int> ::iterator it=adjacent_find(v.begin(),v.end());
-    cout<<*it;
+    while(it!=v.end())
+    {
+        cout<<"Repeated is "<<*it<<endl;
+        it=adjacent_find(++it,v.end());
 
+    }
 
 }
